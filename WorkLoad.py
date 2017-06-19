f3e5094ad5f8d4f783a2fe740c75926d00374e06
Chrisitan Nelson <christian.nelson@eit.lth.se>
2017-06-19 12:03:34 +0200

    Now WLGenerator works! Needed to change seed for random number generator

--- WorkLoad.py
+++ WorkLoad.py
@@ -1,16 +1,16 @@
 import requests
 from multiprocessing import Process, Queue
 import os, time
-from numpy.random import exponential, choice
+from numpy.random import exponential, choice, seed
 import logging
+import shutil
 
 
 SERVICE_URL = 'http://localhost:5000'
 
 
 class WLGenerator:
-    # http://docs.python-requests.org/en/latest/api/#requests.Response
-    # http://docs.python-guide.org/en/latest/writing/logging/
+
     def __init__(self, host, n_users, think_time):
         self.host = host
         self.n_users = n_users
@@ -18,8 +18,8 @@ class WLGenerator:
 
     def run_wg(self):
         ps = []
-        for _ in range(self.n_users):
-            u = User(self.host, self.think_time)
+        for pid in range(self.n_users):
+            u = User(self.host, self.think_time, pid)
             u.start()
             ps.append(u)
 
@@ -28,24 +28,49 @@ class WLGenerator:
 
 
 class User(Process):
-    def __init__(self, host, think_time, timeout = 2):
-        super(Process, self).__init__()
-        self.log = logging.getLogger("Process ID {}".format(os.getpid()))
+    # http://docs.python-requests.org/en/latest/api/#requests.Response
+    # http://docs.python-guide.org/en/latest/writing/logging/
+    # https://docs.python.org/3.6/library/logging.html
+    # https://stackoverflow.com/questions/303200/how-do-i-remove-delete-a-folder-that-is-not-empty-with-python#303225
+
+    def __init__(self, host, think_time, pid, timeout = 2):
+        super().__init__()
+        self._set_up_logger(pid)
         self.host = host
         self.think_time = think_time
         self.timeout = timeout
+        self.log.info('{}'.format(os.getpid()))
+        seed()
+
+    def _set_up_logger(self, pid):
+        self.log = logging.getLogger("Process{}".format(pid))
+
+        formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
+        file_handle = logging.FileHandler('logs/process{}.log'.format(pid))
+        handler = logging.StreamHandler()
+
+        handler.setFormatter(formatter)
+        file_handle.setFormatter(formatter)
+
+        self.log.addHandler(handler)
+        self.log.addHandler(file_handle)
+        self.log.setLevel(logging.DEBUG)
 
     def run(self):
         while True:
             try:
+                t = exponential(1.0/self.think_time)
+                self.log.info('sleeping for {} seconds'.format(t))
+                time.sleep(t)
+
                 self.send_request_to_server()
+
             except requests.exceptions.HTTPError as e:
-                self.log = ""
-                # Store http errors and try again
-                #self.log.error(e)
+                self.log.error(e)
 
     def send_request_to_server(self):
-        time.sleep(exponential(1.0/self.think_time))
 
         #Send request for available movies
         r = requests.get(self.host + '/api/v1/movies', timeout = self.timeout)
@@ -54,21 +79,21 @@ class User(Process):
         movie_uri = self.random_choice_movie(r.json())
 
         r = requests.get(movie_uri, timeout = self.timeout)
-        #self.log.info("Requested {} for conversion, response {}".format(my_video, r.status_code))
+        self.log.info("Requested {} for conversion, response {}".format(movie_uri, r.status_code))
         r.raise_for_status()
 
         job_uri = r.headers['location']
 
-        #Poll for status
+        #Poll job status
         while True:
-            time.sleep(0.1)
-            #self.log.debug("Requesting status after {} seconds of wait".format(self.poll_wait))
+            time.sleep(0.5)
+            self.log.debug('Polling job status')
             r = requests.get(job_uri, timeout = self.timeout)
             r.raise_for_status()
             status_msg = r.json()
 
             if status_msg['resolution'] != "null":
-                #self.log.info("Requested finished with resolution {}".format(status_msg['resolution']))
+                self.log.info("Requested finished : resolution {}".format(status_msg['resolution']))
                 break
 
 
@@ -82,5 +107,12 @@ class User(Process):
 
 
 if __name__ == '__main__':
-    WG = WLGenerator(SERVICE_URL, 10, 1)
+
+    log_path = 'logs'
+    if os.path.exists(log_path):
+        shutil.rmtree(log_path, ignore_errors=True)
+
+    os.mkdir(log_path)
+
+    WG = WLGenerator(SERVICE_URL, 2, 1)
     WG.run_wg()
