4918791354d0e33a6dd7502da62e75fefc4c8a0f
afabijan <aleksander.fabijan@me.com>
2017-10-17 14:03:26 +0200

    updated the worker

--- encode_worker/worker.py
+++ encode_worker/worker.py
@@ -3,19 +3,24 @@ import pika
 from optparse import OptionParser
 import ConfigParser
 import shlex
-from subprocess import call, STDOUT, DEVNULL
+from subprocess import call, STDOUT
+import os
 
 
 def callback(ch, method, properties, body):
 	print(" [x] Received %r" % body)
-	# Update the statust to PROCESSING
-	# Download the movie from the SWIFT object storage
+	
+	# Query the Mongo Database for information about the new movie
+	# Update the statust to PROCESSING of the correct movie
+	# Download the movie from the SWIFT object storage and save it as source 
 
 	cmd = """mencoder %s -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=3000
              -oac copy -o %s""" % (source, dest)
 	print("Converting video file")
 	
-	call(shlex.split(cmd), stdout=DEVNULL, stderr=STDOUT)
+	call(shlex.split(cmd), stdout=os.devnull, stderr=STDOUT)
+
+	
 
 	# upload converted video to SWIFT 
 	# Update the statust to DONE
@@ -45,8 +50,8 @@ if __name__=="__main__":
 		connection["server"] = config.get('rabbit', 'server')
 		connection["port"] = int(config.get('rabbit', 'port'))
 		connection["queue"] = config.get('rabbit', 'queue')
-		connection["username"] = config.get('rabbit', 'username')
-		connection["password"] = config.get('rabbit', 'password')
+		connection["username"] = config.get('user1', 'username')
+		connection["password"] = config.get('user1', 'password')
 		receive(connection_info=connection)
 	else:
 		# e.g. python backend.py -c credentials.txt
